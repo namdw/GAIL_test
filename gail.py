d4be63319ad72f00deec0eec45be1b342fc58725
namdw <dnjsxodp@gmail.com>
2018-01-15 17:56:31 +0900

    more updates

--- gail.py
+++ gail.py
@@ -5,11 +5,8 @@
 import numpy as np
 import tensorflow as tf 
 import matplotlib.pyplot as plt 
-
+from collections import deque
 import gym
-
-# env = gym.make('Cartpole-v0')
-learning_rate = 1e-2
 
 class Dnet:
 	def __init__(self, session, input_size, output_size, name):
@@ -20,7 +17,7 @@ class Dnet:
 
 		self._build_net()
 
-	def _build_net(self, h1_size=100, h2_size=100, lr=learning_rate):
+	def _build_net(self, h1_size=100, h2_size=100, lr=1e-2):
 		with tf.variable_scope(self.name):
 			self._traj = tf.placeholder(tf.float32, [None, self.input_size])
 			self._trajE = tf.placeholder(tf.float32, [None, self.input_size])
@@ -40,7 +37,7 @@ class Dnet:
 		self._lossD = -tf.reduce_mean(tf.log(self._D)+tf.log(1-self._DE))
 		self._trainD = tf.train.AdamOptimizer(learning_rate=lr).minimize(self._lossD)
 
-	def predict(self, state, action, expt):
+	def discriminate(self, state, action, expt):
 		traj = np.reshape(np.append(state, action), [1, self.input_size])
 		if expt:
 			return self.session.run(self._DE, feed_dict={self._trajE: traj})
@@ -61,7 +58,7 @@ class PInet:
 
 		self._build_net()
 
-	def _build_net(self, h1_size=100, h2_size=100, lr=learning_rate, lamb=0.1):
+	def _build_net(self, h1_size=100, h2_size=100, lr=1e-2, lamb=0.1):
 		with tf.variable_scope(self.name):
 			self._state = tf.placeholder(tf.float32, [None, self.input_size])
 			self._action = tf.placeholder(tf.float32, [None, self.output_size])
@@ -80,9 +77,9 @@ class PInet:
 		self._lossPI = -tf.reduce_mean(tf.log(self._pa)*tf.log(self._D)-lamb*tf.log(self._pa))
 		self._trainPI = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(self._lossPI)
 
-	def predict(self, state, action_mat):
+	def policyAt(self, state):
 		x = np.reshape(state, [1, self.input_size])
-		return self.session.run(self._pa, feed_dict={self._state: x, self._action:action_mat})
+		return self.session.run(self._PI, feed_dict={self._state: x})
 		
 
 	def update(self, state, action_mat, D):
@@ -92,14 +89,36 @@ class PInet:
 
 def main():
 	env = gym.make('CartPole-v0')
+	
 	input_size_D = env.observation_space.shape[0] + env.action_space.n
 	output_size_D = 1
 	input_size_P = env.observation_space.shape[0]
 	output_size_P = env.action_space.n
+
+	max_episodes = 10
+
+	# print(env.action_space)
+
 	with tf.Session() as sess:
 		discriminator = Dnet(sess, input_size_D, output_size_D, "D")
 		policy = PInet(sess, input_size_P, output_size_P, "policy")
 		tf.global_variables_initializer().run()
+
+		for episode_num in range(max_episodes):
+			done = False
+			state = env.reset()
+
+			step_counter = 0
+			while True:
+				step_counter += 1
+				env.render()
+				action_prob = policy.policyAt(state)[0]
+				a = 0 if(action_prob[0] > action_prob[1]) else 1
+				
+				state, reward, done, _ = env.step(a)
+
+				if done or step_counter > 300:
+					break
 
 if __name__ == "__main__":
 	main()
